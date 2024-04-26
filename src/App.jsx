d0ebe58f18cb8159b534dc4f4bd6122402e11a87
Juan <j.tejeda46@yahoo.com>
2024-04-26 17:07:02 -0500

    Added button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Header from "./Header"
 import Footer from "./Footer"
 import Card from "./Card"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return(
     <>
-      <Header />
-      <Card />
-      <Footer />
+        <Header />
+        <Card firstName="Juan" lastName="Tejeda" career="Software Engineer" isEmployed={false}/>
+        <Card firstName="Sherley" lastName="Ly" career="Registered Nurse" isEmployed={true}/>
+        <Footer />
     </>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,15 @@
 import ProfilePic from "./assets/profile-pic.jpg"
+import Button from "./Button";
 
-function Card(){
+function Card(props){
 
 	return(
 		<div className="card">
 			<img className="card-image" src={ProfilePic} alt="profile pic" />
-			<h2 className="card-title">Juan Tejeda</h2>
-			<p className="card-text">Software Engineer</p>
+			<h2 className="card-title">{props.firstName} {props.lastName}</h2>
+			<p className="card-text">{props.career}</p>
+			<p className="card-text">Employment Status: {props.isEmployed ? 'Employed' : 'Unemployed'}</p>
+			<Button />
 		</div>
 	);
 }
