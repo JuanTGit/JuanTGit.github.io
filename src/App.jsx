62ef237cb591dcc0704c87803b6f78ffb27648af
Juan <j.tejeda46@yahoo.com>
2024-04-29 17:00:57 -0500

    Added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import AppNavbar from "./Navbar"
 import Footer from "./Footer"
 import { Routes, Route } from "react-router-dom"
-import About from "./pages/about"
+import About from "./components/About/About"
+import Home from "./components/Home/Home"
 
 function App() {
 
   return(
     <>
         <AppNavbar />
-         <Routes>
-            <Route path="/about" element={<About />}/>
-         </Routes>
+            <Routes>
+                <Route path="/about" element={<About />}/>
+                <Route path="/home" element={<Home />}/>
+            </Routes>
         <Footer />
     </>
   )
