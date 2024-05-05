386c6e06397af705a3a7b1ac18229186139c594a
Juan <j.tejeda46@yahoo.com>
2024-05-05 18:00:37 -0500

    Updated resume xm

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,7 +3,6 @@ import resume from "../assets/resume.jpg"
 function Resume(){
     return(
         <div>
-            <h1 className="text-center">Resume</h1>
             <div className="container d-flex justify-content-center">
                 <img className="col-8" src={resume} alt="" />
             </div>
