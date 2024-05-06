6e430a2afa5ac5b2e550defcc75442e632f5e38b
Juan <j.tejeda46@yahoo.com>
2024-05-06 07:51:40 -0500

    Added projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,29 @@
 
 function Projects(){
     return(
-        <div>
+        <div className="container">
             <h1 className="text-center">Projects</h1>
+            {/* Ecommerce Project */}
+            <div className="row">
+                <div className="col-6">
+                    <a href="https://flask-setup.onrender.com">Link</a>
+                </div>
+                <div className="col-6">
+                    <h1 className="text-center">Ecommerce Project</h1>
+                </div>
+            </div>
+
+            {/* OSRS GE API Project */}
+            <div className="row">
+                <div className="col-6">
+                    <h1 className="text-center">OSRS API Project</h1>
+                </div>
+                <div className="col-6">
+                    <a href="https://osrs-ge-api.onrender.com">Link</a>
+                </div>
+            </div>
+
+
         </div>
     );
 }
