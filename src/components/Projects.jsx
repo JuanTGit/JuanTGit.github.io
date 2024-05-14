165976ba6aee7df00bb83d02689def0fac3645f6
Juan <j.tejeda46@yahoo.com>
2024-05-13 21:43:23 -0500

    Added js for opening links in new tab

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,11 @@ import osrsImg from "../assets/osrs-api.png"
 import ecomImg from "../assets/ecommerce.png"
 
 function Projects(){
+    const openInNewTab = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+      }
+
     return(
         <div className="container">
             <h1 className="text-left mt-5">Projects</h1>
@@ -14,7 +19,7 @@ function Projects(){
                 {/* txt */}
                 <div className="col-12 col-sm-6 mt-5"  id="project-card">
                     <h1 className="text-left">Ecommerce Store</h1>
-                    <a href="https://flask-setup.onrender.com">Link</a>
+                    <a className="btn btn-primary" onClick={() => openInNewTab("https://flask-setup.onrender.com")}>Visit Site</a>
                 </div>
             </div>
 
@@ -27,7 +32,7 @@ function Projects(){
                 {/* txt */}
                 <div className="col-12 col-sm-6" id="project-card">
                     <h1 className="text-left mt-5">OSRS API</h1>
-                    <a href="https://osrs-ge-api.onrender.com">Link</a>
+                    <a className="btn btn-primary" onClick={() => openInNewTab("https://osrs-ge-api.onrender.com")}>Visit Site</a>
                 </div>
             </div>
 
