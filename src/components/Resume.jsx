017a94c37738a4da265866152791a5edb712e8c6
Juan <j.tejeda46@yahoo.com>
2024-07-12 20:14:31 -0500

    Added download to cv

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,10 +2,11 @@ import resume from "../assets/resume.jpg"
 
 function Resume(){
     return(
-        <div>
-            <h1 className="text-center my-5" id="project-title">Resume</h1>
-            <div className="container d-flex justify-content-center">
-                <img className="col-12 mb-3" src={resume} alt="" />
+        <div className="container d-flex flex-column align-items-center justify-content-center min-vh-100">
+            <h1 className="text-center mt-5" id="project-title">Resume</h1>
+            <a className="my-3 btn btn-primary" href="/resume.pdf" download>Download CV</a>
+            <div className="d-flex justify-content-center">
+                <img className="col-12 col-md-7 mb-3" src={resume} alt="Resume" />
             </div>
         </div>
     );
