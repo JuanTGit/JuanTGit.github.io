27e3883b78af3097ef41943e6fe385ad0779c552
Juan <j.tejeda46@yahoo.com>
2024-07-12 21:40:43 -0500

    fixed resume vh on mobile devices

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,7 +2,7 @@ import resume from "../assets/resume.jpg"
 
 function Resume(){
     return(
-        <div className="container d-flex flex-column align-items-center justify-content-center min-vh-100">
+        <div className="container d-flex flex-column align-items-center justify-content-center">
             <h1 className="text-center mt-5" id="project-title">Resume</h1>
             <a className="my-3 btn btn-primary" href="/resume.pdf" download>Download CV</a>
             <div className="d-flex justify-content-center">
