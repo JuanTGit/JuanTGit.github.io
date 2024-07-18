ff5f91cb1c2e0a4cc2e252317ce243228246d650
Juan <j.tejeda46@yahoo.com>
2024-07-18 18:42:47 -0500

    added hashrouter instead of browser router for ghpages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import AppNavbar from "./components/Navbar"
 import Footer from "./components/Footer"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./components/Home"
 import Resume from "./components/Resume"
 import Projects from "./components/Projects"
@@ -9,30 +9,33 @@ import { useState } from "react"
 
 function App() {
   // typeWriter State
-  const [currentStringIndex, setCurrentStringIndex] = useState(0);
-  const [letter, setLetter] = useState(0);
-  const [displayedTexts, setDisplayedTexts] = useState(["", "", ""]);
+    const [currentStringIndex, setCurrentStringIndex] = useState(0);
+    const [letter, setLetter] = useState(0);
+    const [displayedTexts, setDisplayedTexts] = useState(["", "", ""]);
 
-  return(
-    <>
-      <Router>
-      <AppNavbar />
-          <Routes>
-              <Route path="/" element={<Home 
-                                        currentStringIndex={currentStringIndex}
-                                        letter={letter}
-                                        displayedTexts={displayedTexts}
-                                        setCurrentStringIndex={setCurrentStringIndex}
-                                        setLetter={setLetter}
-                                        setDisplayedTexts={setDisplayedTexts}/>}/>
-              <Route path="contact" element={<Contact />}/>
-              <Route path="projects" element={<Projects />}/>
-              <Route path="resume" element={<Resume />}/>
-          </Routes>
-      </Router>
-      <Footer />
-    </>
-  )
+    return(
+        <>
+        <Router>
+        <AppNavbar />
+            <Routes>
+                <Route path="/" element={
+                    <Home 
+                        currentStringIndex={currentStringIndex}
+                        letter={letter}
+                        displayedTexts={displayedTexts}
+                        setCurrentStringIndex={setCurrentStringIndex}
+                        setLetter={setLetter}
+                        setDisplayedTexts={setDisplayedTexts}
+                    />}
+                />
+                <Route path="contact" element={<Contact />}/>
+                <Route path="projects" element={<Projects />}/>
+                <Route path="resume" element={<Resume />}/>
+            </Routes>
+        </Router>
+        <Footer />
+        </>
+    )
 }
 
 export default App
