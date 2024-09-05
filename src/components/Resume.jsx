6efd7bbadb61b38adbf2cbe4efeb0d1aab5cc464
Juan <j.tejeda46@yahoo.com>
2024-09-05 13:44:45 -0500

    updated resume/download

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,10 +1,10 @@
-import resume from "../assets/resume.jpg"
+import resume from "../assets/Resume.jpg"
 
 function Resume(){
     return(
         <div className="container d-flex flex-column align-items-center justify-content-center">
             <h1 className="text-center mt-5" id="project-title">Resume</h1>
-            <a className="my-3 btn btn-primary" href="/resume.pdf" download>Download CV</a>
+            <a className="my-3 btn btn-primary" href="/Resume.pdf" download>Download CV</a>
             <div className="d-flex justify-content-center">
                 <img className="col-12 col-md-7 mb-3" src={resume} alt="Resume" />
             </div>
